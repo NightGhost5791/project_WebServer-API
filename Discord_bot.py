4f319d4305305217bb725594f38faa7273ae5e01
NightGhost5791 <77852925+NightGhost5791@users.noreply.github.com>
2022-05-06 23:21:57 +0300

    Почти готово
    Добавил коментарии, API, работу с изображениями, создал пока пустую БД. Завтра дополню код для БД и выложу на хостинге

--- Discord_bot.py
+++ Discord_bot.py
@@ -1,19 +1,19 @@
 import discord 
+
 from discord_components import DiscordComponents, ComponentsBot, Button
 import discord.ext.commands as commands
-from discord_components.interaction import Interaction
+import random 
+# Импортируемые библиотеки
 
-import random
-
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!") # Префикс для активации команд
 DiscordComponents(client)
 
 player1 = ""
 player2 = ""
 turn = ""
 GameOver = True
-
 board = []
+# Переменные
 
 WinningConditions = [[0, 1, 2],
                      [3, 4, 5],
@@ -23,25 +23,38 @@ WinningConditions = [[0, 1, 2],
                      [2, 5, 8],
                      [0, 4, 8],
                      [2, 4, 6]]
+# Комбинации победы для игры "Крестики-Нолики"
 
-@client.command()
+@client.command() # Добавление команды с названием instruction
+async def instruction(ctx):
+    await ctx.send('1. !tictactoe Игрок1 Игрок2\n Начать играть в "Крестики-Нолики"')
+    await ctx.send('2. !wordgame Игрок1 Игрок2\n Начать "Игру в слова"')
+    # Вывод инструкции по боту в чат
+
+@client.command() # Добавление команды с названием tictactoe для запуска игры "Крестики-Нолики"
 async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
     global count, player1, player2, turn, GameOver
-
-    if GameOver:
+    # Глобальные переменные 
+    
+    if GameOver: # Условие окончания игры
         global board
         board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
                  ":white_large_square:",":white_large_square:",":white_large_square:",
                  ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        # Игровое поле
         turn = ""
         GameOver = False
         count = 0
-
         player1 = p1
         player2 = p2
-
         line = ""
-        for x in range(len(board)):
+        # Переменные
+        
+        emb = discord.Embed() # Создание заставки
+        emb.set_image(url = "http://sun9-31.userapi.com/impf/0_pvt3wzfxqDQueaORCb7tbC7xtke8oc3T4JvQ/u8upEVZwQUo.jpg?size=1590x530&quality=95&crop=230,0,1200,400&sign=e088936cf44b66d169316dab5a740c08&type=cover_group")
+        await ctx.send(embed = emb) 
+
+        for x in range(len(board)): # Создание игрового поля
             if x == 2 or x == 5 or x == 8:
                 line += " " + board[x]
                 await ctx.send(line)
@@ -49,7 +62,7 @@ async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
             else:
                 line += " " + board[x]
 
-        num = random.randint(1, 2)
+        num = random.randint(1, 2) # Выбор игрока, который будет ходить первым
         if num == 1:
             turn = player1
             await ctx.send("<@" + str(player1.id) + "> Ходит первым")
@@ -58,36 +71,37 @@ async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
             await ctx.send("<@" + str(player2.id) + "> Ходит первым")
 
         await ctx.send(components = [
-            [Button(label="1"), Button(label="2"), Button(label="3")],
-            [Button(label="4"), Button(label="5"), Button(label="6")],
-            [Button(label="7"), Button(label="8"), Button(label="9")]]) 
+            [Button(label=1), Button(label=2), Button(label=3)],
+            [Button(label=4), Button(label=5), Button(label=6)],
+            [Button(label=7), Button(label=8), Button(label=9)]]) # Создание кнопок для упрощённого ввода команд
     else:
-        await ctx.send("Партия ещё не закончилась")
+        await ctx.send("Партия ещё не закончилась") # Ошибка при попытке создания новой партии
 
-@client.event
-async def on_button_click(interaction: Interaction):
-    #print(interaction.component.label)
-    await interaction.send(content=interaction.component.label, ephemeral=False)
-    client.loop.create_task(place(interaction, int(interaction.component.label)))
+@client.event # Событие нажатия на кнопку
+async def on_button_click(ctx):  
+    await client.loop.create_task(place(ctx, int(ctx.component.label)))
+    # Вызов функции при нажатии на кнопку 
 
-@client.command()
+@client.command() # Создание функции для замены пустой клетки поля на крестик или нолик
 async def place(ctx, pos: int):
-    global turn, player1, player2, board, count, GameOver
-
+    global turn, player1, player2, board, count, GameOver 
     if not GameOver:
         mark = ""
+
         if turn == ctx.author:
             if turn == player1:
                 mark = ":regional_indicator_x:"
+
             elif turn == player2:
                 mark = ":o2:"
+            # Установка крестика или нолика 
+
             if 0 < pos < 10 and board[pos - 1] == ":white_large_square:":
                 board[pos - 1] = mark
                 count += 1
-
-                # print the board
+                
                 line = ""
-                for x in range(len(board)):
+                for x in range(len(board)): # Вывод игрового поля
                     if x == 2 or x == 5 or x == 8:
                         line += " " + board[x]
                         await ctx.send(line)
@@ -95,15 +109,14 @@ async def place(ctx, pos: int):
                     else:
                         line += " " + board[x]
 
-                checkWinner(WinningConditions, mark)
-                print(count)
-                if GameOver == True:
+                checkWinner(WinningConditions, mark) # Проверка победителя
+                if GameOver:
                     await ctx.send(mark + " Побеждают!")
-                elif count >= 9:
+                elif count >= 9: # Условие ничьей
                     GameOver = True
                     await ctx.send("Ничья!")
 
-                # switch turns
+                # Смена ходов
                 if turn == player1:
                     turn = player2
                 elif turn == player2:
@@ -116,13 +129,13 @@ async def place(ctx, pos: int):
         await ctx.send("Для начала новой игры введите: !tictactoe Игрок1 Игрок2")
 
 
-def checkWinner(WinningConditions, mark):
+def checkWinner(WinningConditions, mark): # Функция для проверки победителя
     global GameOver
     for condition in WinningConditions:
         if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
             GameOver = True
 
-@tictactoe.error
+@tictactoe.error 
 async def tictactoe_error(ctx, error):
     print(error)
     if isinstance(error, commands.MissingRequiredArgument):
@@ -136,6 +149,62 @@ async def place_error(ctx, error):
         await ctx.send("Выберите клетку")
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Введите целое число")
+# Создание функций для реакции на ошибки
 
-client.run("TOKEN")
+@client.command() # Добавление функции wordgame для запуска "Слова из Слов"
+async def wordgame(ctx, p1: discord.Member, p2: discord.Member):
+    global count, player1, player2, turn, GameOver
+    if GameOver:
+        turn = ""
+        GameOver = False
+        count = 0
+        player1 = p1
+        player2 = p2
+        num = random.randint(1, 2) # Выбор игрока, который будет ходить первым
+        
+        emb = discord.Embed() # Создание заставки
+        emb.set_image(url = "http://i.otzovik.com/objects/b/920000/915028.png")
+        await ctx.send(embed = emb) 
+
+        if num == 1:
+            turn = player1
+            await ctx.send("<@" + str(player1.id) + "> Ходит первым")
+        elif num == 2:
+            turn = player2
+            await ctx.send("<@" + str(player2.id) + "> Ходит первым")
+
+    else:
+        await ctx.send("Игра ещё не закончилась")
+
+@client.command() # Создание функции для ввода слова
+async def word(ctx, wrd):
+    global turn, player1, player2, count, GameOver, old_word
+    if not GameOver:
+        if turn == ctx.author:
+            if count >= 1:
+                if old_word[-1].lower() != wrd[0].lower(): # Сравнение первой буквы первого слова с последней буквы старого слова
+                    await ctx.send('Игра окончена\n---Счёт:' + str(count))
+                    GameOver = True
+
+            if not GameOver:
+                if turn == player1:
+                    await ctx.send(str(player2) + ' Пишет слово на букву ' + wrd[-1].upper())
+                    count += 1
+                elif turn == player2:
+                    await ctx.send(str(player1) + ' Пишет слово на букву ' + wrd[-1].upper())
+                    count += 1
+                # Подсказка для следующего игрока
+                old_word = wrd # Замена старого слова на новое после проверки
+                
+                # Смена ходов
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+        else:
+            await ctx.send("Это не ваш ход!")
+    else:
+        await ctx.send("Для начала новой игры введите: !wordgame Игрок1 Игрок2")
+
+client.run("OTQ5OTgyMjg4NjQzNDg5ODE0.YiSR8w.011_NreopoTk-_rIARSZhR_k7ro") # Запуск бота по текущему токену 
 
